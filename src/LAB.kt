b05ef1a29af4d2435f55ae6aed6906159eb38a87
Mert Selcuk <mert.selcuk@morhipo.com>
2019-02-10 21:01:03 +0300

    #Mert_Selçuk: Graduate pass value calculated and function wrote with name and surname show main function

--- src/LAB.kt
+++ src/LAB.kt
@@ -12,3 +12,28 @@
 // TODO(3) Kullanıcıdan vize notunu alın.
 // TODO(4) Kullanıcıdan final notunu alın.
 // TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+
+
+fun main() {
+
+    println("Adınızı giriniz : ")
+    val name = readLine()
+
+    println("Soyadınızı giriniz : ")
+    val surName = readLine()
+
+    println("Vize notunuzu giriniz : ")
+    val midtermPoint = readLine()
+
+    println("Final notunuzu giriniz : ")
+    val finalPoint = readLine()
+
+    print("Ortalamanız : ")
+    print(midtermPoint!!.toDouble() extAverage (finalPoint!!.toDouble()))
+
+}
+
+infix fun Double.extAverage(passPoint : Double) : Double = this * 0.4 +  passPoint * 0.6
+
+
+
