793d68bc728e3297f746d03245936fe2f090ab20
Betul Sandikci <btlsndkc@gmail.com>
2019-02-10 13:27:59 +0300

    First Commit With Love <3

--- src/LAB.kt
+++ src/LAB.kt
@@ -12,3 +12,14 @@
 // TODO(3) Kullanıcıdan vize notunu alın.
 // TODO(4) Kullanıcıdan final notunu alın.
 // TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+fun main() {
+
+    val userName : String? = readLine()
+    val userSurname : String? = readLine()
+    val vizePoint: String? = readLine()
+    val finalPOint: String? = readLine()
+
+
+
+
+}
