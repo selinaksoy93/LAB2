21eb42adb0585342cab8d333dbd53e242dfe1c7a
Emirhan Karaağaç <ekaraagac52@gmail.com>
2019-02-10 14:08:36 +0300

    Dont Look

--- src/LAB.kt
+++ src/LAB.kt
@@ -12,3 +12,23 @@
 // TODO(3) Kullanıcıdan vize notunu alın.
 // TODO(4) Kullanıcıdan final notunu alın.
 // TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+
+fun main() {
+    print("Adınızı yazınız")
+    var name = readLine()
+
+    print("Soyadınızı yazın")
+    var surname = readLine()
+
+    print("Vize notunuzu yazın")
+    var midTerm = readLine()
+
+    print("Final notunuzu giriniz")
+    var final = readLine()
+
+
+}
+
+infix fun Int.extCalculate(final: Int): Int {
+    val mid
+}
