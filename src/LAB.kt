3985f54eab84973b008ae67d9fba5e14123b5cd1
Onur TAŞDEMİR <onurtaademir@gmail.com>
2019-02-10 14:02:55 +0300

    asdsad

--- src/LAB.kt
+++ src/LAB.kt
@@ -12,3 +12,27 @@
 // TODO(3) Kullanıcıdan vize notunu alın.
 // TODO(4) Kullanıcıdan final notunu alın.
 // TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+
+
+fun main(args: Array<String>) {
+    print("Kullanıcı adınızı girin :")
+    val userName = readLine()
+    print("Kullanıcı soyadınızı girin :")
+    val userSurname = readLine()
+    print("Kullanıcı vize notunuzu girin :")
+    val vize = readLine()
+    print("Kullanıcı final notunuzu girin :")
+    val final = readLine()
+    println(vize!!.toInt() concate (final!!.toInt()))
+
+
+}
+
+infix fun Int.concate(Final: Int): String {
+    val ort: Double = (this * (0.4) + Final * (0.6)) / 2
+    if (ort <= 50) {
+        return "Başarısız."
+    } else {
+        return "Başarılı."
+    }
+}
