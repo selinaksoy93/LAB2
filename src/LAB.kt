05dd78dee598131c141aa4e590a7d58e24c7f33e
Cansu Aktaş <34041050+cyeksan@users.noreply.github.com>
2019-02-10 14:01:23 +0300

    "Lab-2 Commit has been completed."

--- src/LAB.kt
+++ src/LAB.kt
@@ -7,8 +7,34 @@
 //│ 2019-02-10 - 13:14          │
 //└─────────────────────────────┘
 
-// TODO(1) Kullanıcıdan adını alın.
-// TODO(2) Kullanıcıdan soyadını alın.
-// TODO(3) Kullanıcıdan vize notunu alın.
-// TODO(4) Kullanıcıdan final notunu alın.
+fun main(args: Array<String>) {
+
+    // TODO(1) Kullanıcıdan adını alın.
+    print("Adınızı giriniz: ")
+    val userName = readLine()
+
+
+    // TODO(2) Kullanıcıdan soyadını alın.
+    print("Soyadınızı giriniz: ")
+    val userSurname = readLine()
+
+    // TODO(3) Kullanıcıdan vize notunu alın.
+    print("Vize notunuzu giriniz: ")
+    val midtermNote = readLine()!!.toInt()
+
+    // TODO(4) Kullanıcıdan final notunu alın.
+    print("Final notunuzu giriniz: ")
+    val finalNote = readLine()!!.toInt()
+
+    println("$userName $userSurname")
+    print("Geçiş notunuz: ${midtermNote extCalculateAverage (finalNote)}")
+}
+
 // TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+infix fun Int.extCalculateAverage(finalNote: Int): Int {
+
+    return (this * 0.4 + finalNote * 0.6).toInt()
+}
+
+
+
