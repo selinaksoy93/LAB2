a33b25bce735fab2b5b11c581c843492c2fd420d
Mithat Sinan Sarı <m.sinan.sari@gmail.com>
2019-02-10 13:56:26 +0300

    Vize ve Final ortalama alan method yazıldı

--- src/LAB.kt
+++ src/LAB.kt
@@ -12,3 +12,41 @@
 // TODO(3) Kullanıcıdan vize notunu alın.
 // TODO(4) Kullanıcıdan final notunu alın.
 // TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+
+fun main() {
+    print("Adınız: ")
+    val isim = readLine() ?: ""
+    print("Soyadınız: ")
+    val soyisim = readLine() ?: ""
+
+    try {
+        print("Vize notunuz: ")
+        val vize = readLine()?.toInt() ?: 0
+
+        print("Final notunuz: ")
+        val final = readLine()?.toInt() ?: 0
+
+        if (vize !in 0..100 || final !in 0..100) throw GecersizSayiException()
+
+        print("$isim $soyisim ${vize extOrtalama final}")
+
+    } catch (exception: NumberFormatException) {
+        print("Geçersiz bir sayı girdiniz")
+    } catch (exception: GecersizSayiException) {
+        print("Sonuçlar 0 ile 100 arasında olmalı")
+    }
+}
+
+
+/**
+ * Int.extOrtalama(final)
+ * this = Vize
+ * final = Final
+ *
+ * return value = "Sonuç {vize*0.4 + final*0.6}"
+ */
+infix fun Int.extOrtalama(final: Int): String {
+    return "Sonuç ${this * 0.4 + final * 0.6}"
+}
+
+class GecersizSayiException() : Exception()
