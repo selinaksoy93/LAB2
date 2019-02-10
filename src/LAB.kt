933f6c467894f778a93199c062002448ff150591
Can SEVİN <mcancaglak@hotmail.com>
2019-02-10 14:04:44 +0300

    Mahmutcan Sevin

--- src/LAB.kt
+++ src/LAB.kt
@@ -1,14 +1,24 @@
-// Code with ❤️
-//┌─────────────────────────────┐
-//│ Created by Gökhan ÖZTÜRK    │
-//│ ─────────────────────────── │
-//│ GokhanOzturk@AndroidEdu.IO  │            
-//│ ─────────────────────────── │
-//│ 2019-02-10 - 13:14          │
-//└─────────────────────────────┘
+fun main() {
+    print("Adınız?")
+    val name: String? = readLine()
+    print("Soyadınız?")
+    val surname: String? = readLine()
+    print("Vize?")
+    val vize = readLine()
+    print("Final?")
+    val final = readLine()
 
-// TODO(1) Kullanıcıdan adını alın.
-// TODO(2) Kullanıcıdan soyadını alın.
-// TODO(3) Kullanıcıdan vize notunu alın.
-// TODO(4) Kullanıcıdan final notunu alın.
-// TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+    println(vize!!.toInt() not (final!!.toInt()))
+}
+
+infix fun Int.not(final: Int): String {
+    val total = (this * 40 / 100 + final * 60 / 100)
+    return when (total) {
+        in 90..100 -> "Your total:$total Note:AA"
+        in 80..89 -> "Your total:$total Note:BB"
+        in 70..79 -> "Your total:$total Note:CC"
+        in 60..69 -> "Your total:$total Note:DD"
+        in 50..59 -> "Your total:$total Note:EE"
+        else -> "kaldın"
+    }
+}
