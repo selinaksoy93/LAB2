82cba5160211bb953fc001250568d66643d8d1a0
Onur Unver <onurunver@gmail.com>
2019-02-10 13:37:22 +0300

    Java JDK versiyon farkı alıyorum, run etmiyor..

--- src/LAB.kt
+++ src/LAB.kt
@@ -12,3 +12,26 @@
 // TODO(3) Kullanıcıdan vize notunu alın.
 // TODO(4) Kullanıcıdan final notunu alın.
 // TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+
+fun main (){
+    print("kullanıcı adını giriniz:")
+    val userName:String? = readLine()
+
+    print("kullanıcı soyadını giriniz:")
+    val userSurName:String? = readLine()
+
+    print("vize notunuzu giriniz:")
+    val vizeNotu:String? = readLine()
+
+    print("final notunuzu giriniz:")
+    val finalNotu:String? = readLine()
+
+    println(concatenate(userName, userSurName))
+
+    println(" ${calcGPA(vizeNotu!!.toDouble(), finalNotu!!.toDouble())}")
+}
+
+
+fun calcGPA (vize:Double , final:Double) : Double =(vize * .4) + (final * .6)
+
+fun concatenate(s1:String?, s2:String?): String = "$s1 + $s2"
