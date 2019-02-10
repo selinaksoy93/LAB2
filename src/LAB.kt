bc8280b7d8758428b39f2727088f69e8ae4eab58
Emre Berk <ebakirmak@gmail.com>
2019-02-10 13:48:22 +0300

    Kullanıcı vize ve final notu infix methodu ile hesaplandı.

--- src/LAB.kt
+++ src/LAB.kt
@@ -12,3 +12,25 @@
 // TODO(3) Kullanıcıdan vize notunu alın.
 // TODO(4) Kullanıcıdan final notunu alın.
 // TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+
+fun main() {
+    print("Kullanıcı adınızı giriniz:")
+    val userName: String? = readLine()
+
+    print("Kullanıcı soyadını giriniz:")
+    val userSurname: String? = readLine()
+
+    print("Kullanıcı vize notu:")
+    val vizeScore: Double = readLine()!!.toDouble()
+
+    print("Kullanıcı final notu:")
+    val finalScore: Double = readLine()!!.toDouble()
+
+    print(userName?.extName((((userSurname!!)))))
+    print(vizeScore extCalculator (finalScore))
+
+
+}
+
+infix fun Double.extCalculator(final: Double): Double = this * 0.4 + final * 0.6
+infix fun String.extName(surname: String): String = "$this $surname"
