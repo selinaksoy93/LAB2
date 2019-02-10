a3fb82539b0ca4d4aa4c5cf6da4397ca3592f777
Hadi Albayram <albayramidah@gmail.com>
2019-02-10 14:02:06 +0300

    Lab uygulaması

--- src/LAB.kt
+++ src/LAB.kt
@@ -12,3 +12,19 @@
 // TODO(3) Kullanıcıdan vize notunu alın.
 // TODO(4) Kullanıcıdan final notunu alın.
 // TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+fun main(args: Array<String>) {
+    print("Adınızı yazın")
+    val name = readLine()
+    println("Soyadınızı yazın")
+    val Surname = readLine()
+    println("Vize Notunuzu girin")
+    val Vize = readLine()!!.toInt()
+    println("Final notunuzu girin")
+    val final = readLine()!!.toInt()
+    print("$name $Surname geçiş notunuz=")
+    print(Vize extCalculate final)
+
+}
+
+infix fun Int.extCalculate(final: Int) = this * 0.4 + final * 0.6
+
