c6593f43da8262028c533f905f36b8e29afe30a0
Gökhan ÖZTÜRK <GokhanOZTURK@AndroidEdu.IO>
2019-02-10 13:15:31 +0300

    First commit with Love <3

---
+++ src/LAB.kt
@@ -0,0 +1,14 @@
+// Code with ❤️
+//┌─────────────────────────────┐
+//│ Created by Gökhan ÖZTÜRK    │
+//│ ─────────────────────────── │
+//│ GokhanOzturk@AndroidEdu.IO  │            
+//│ ─────────────────────────── │
+//│ 2019-02-10 - 13:14          │
+//└─────────────────────────────┘
+
+// TODO(1) Kullanıcıdan adını alın.
+// TODO(2) Kullanıcıdan soyadını alın.
+// TODO(3) Kullanıcıdan vize notunu alın.
+// TODO(4) Kullanıcıdan final notunu alın.
+// TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
