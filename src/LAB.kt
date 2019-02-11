18b2880dcdd3475ad5cc203fdee7db8e4b25cbfb
Duygu Turkmen <duyguuturkmen@gmail.com>
2019-02-11 22:12:49 +0300

    LAB2 tamamlandı.

--- src/LAB.kt
+++ src/LAB.kt
@@ -12,3 +12,24 @@
 // TODO(3) Kullanıcıdan vize notunu alın.
 // TODO(4) Kullanıcıdan final notunu alın.
 // TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+
+
+fun main() {
+    print("Lütfen adınızı giriniz:")
+    val isim: String = readLine()!!
+
+    print("Lütfen soyadınızı giriniz:")
+    val soyadi: String = readLine()!!
+
+    print("Lütfen vize notunuzu giriniz :")
+    val vizeNotu = readLine()!!.toInt()
+
+    print("Lütfen final notunuzu giriniz:")
+    val finalNotu = readLine()!!.toInt()
+
+    val sonuc = vizeNotu hesapla finalNotu
+    print("Ortalama: $sonuc")
+
+}
+
+infix fun Int.hesapla(sonuc: Int): Int = ((this * 40 / 100) + (sonuc * 60 / 100))
