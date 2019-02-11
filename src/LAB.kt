79473bd5bb6b9b156cb55ed11f22269cb5750a1e
Ecem SÜREN <ecemsurenn@gmail.com>
2019-02-11 21:37:01 +0300

    Calculated Averages

--- src/LAB.kt
+++ src/LAB.kt
@@ -12,3 +12,27 @@
 // TODO(3) Kullanıcıdan vize notunu alın.
 // TODO(4) Kullanıcıdan final notunu alın.
 // TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+
+
+fun main() {
+    print("kulanıcı adını girin\n")
+    val name: String? = readLine()
+
+    print("kullanıcı soyadını giriniz\n")
+    val soyad: String? = readLine()
+
+    print("vize notu giriniz\n")
+    val vizeNotu = readLine()?.toInt()
+
+    print("final notunu giriniz\n")
+    val finalNotu = readLine()?.toInt()
+
+    print("Ortalamanız: ")
+    vizeNotu?.Average(finalNotu!!)
+
+}
+
+infix fun Int.Average(finalNotu: Int) = print("toplam not:" + (this * 40 / 100 + finalNotu * 60 / 100))
+
+
+
