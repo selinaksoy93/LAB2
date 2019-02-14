dd30ae8c1f7b8aa800ca9c47bc48d7cd56cb1136
esra <sdhf@gmail.com>
2019-02-14 20:17:02 +0300

    Kullanıcıdan ad, soyad, vize notu,final notu alınıp geçme notu hesaplandı

--- src/LAB.kt
+++ src/LAB.kt
@@ -12,3 +12,30 @@
 // TODO(3) Kullanıcıdan vize notunu alın.
 // TODO(4) Kullanıcıdan final notunu alın.
 // TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+
+fun main() {
+    print("Adınızı giriniz: ")
+    val name = readLine()
+    print("Soyadınızı giriniz: ")
+    val surname = readLine()
+    print("Vize notunuzu giriniz: ")
+    val midtermScore = readLine()
+    print("Final notunuzu giriniz: ")
+    val finalScore = readLine()
+    print(midtermScore!!.toInt() calc finalScore!!.toInt())
+}
+
+infix fun Int.calc(finalScore: Int) {
+    val average = (this * 0.4 + finalScore * 0.6)
+    print("Geçme notunuz: $average")
+
+}
+
+
+
+
+
+
+
+
+
