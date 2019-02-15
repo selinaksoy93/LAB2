102a5b43c525b2585e3c562f4c80d0d5243eca1a
bgraknd <bgraknd@gmail.com>
2019-02-15 18:29:48 +0300

    average calculation with infix function

--- src/LAB.kt
+++ src/LAB.kt
@@ -12,3 +12,28 @@
 // TODO(3) Kullanıcıdan vize notunu alın.
 // TODO(4) Kullanıcıdan final notunu alın.
 // TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+
+
+fun main() {
+
+    try {
+        print("Isim: ")
+        val isim = readLine()
+        print("Soyisim: ")
+        val soyIsim = readLine()
+        print("Vize: ")
+        val vize = readLine()!!.toInt()
+        print("Final: ")
+        val final = readLine()!!.toInt()
+
+        if (vize in 0..100 && final in 0..100) {
+            println("$isim $soyIsim ${vize extOrtalama final}")
+        }else throw Exception()
+    }catch(e: Exception) {
+        println("Yanlis bilgi girisi yaptiniz!\nBilgileri dogru bir sekilde tekrar girin...")
+        return main()
+    }}
+
+infix fun Int.extOrtalama(final:Int): String{
+    return "Ortalama: ${this * 0.4 + final * 0.6}"
+}
